5dcccadc701961f3a3c9e4164fb2a5e17c077e17
Abhinav Prasad <psmithman@gmail.com>
2021-11-27 18:35:41 +0530

    Updated Player movement and actions Signed-off-by: Abhinav Prasad <psmithman@gmail.com>

--- Assets/Scripts/MoveMarble.cs
+++ Assets/Scripts/MoveMarble.cs
@@ -9,16 +9,19 @@ public class MoveMarble : MonoBehaviour
     public bool GameStart = false;
     public float growthRate;
     public float maxMarbleSize;
+
+    private float scale;
     // Start is called before the first frame update
     void Start()
     {
         Alive = true;
        // Color randomColor = ;
-        float scale = 1.3f*Random.value + 0.3f;
+        scale = 1.3f*Random.value + 0.3f;
        // GetComponent<Renderer>().material.SetColor("_Color", Color.red);
         //ChangeColor(Agent.Instance.Scale);
         GetComponent<Transform>().localScale *= scale;
         GetComponent<Rigidbody>().mass = scale;
+        scale *= 2;
         
     }
 
@@ -30,15 +33,13 @@ public class MoveMarble : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         direction = direction * -1;
-        if (transform.localScale.x > 3f)
-            return;
         if (GameStart == false) return;
-        if (other.gameObject.tag != "Marble") return;
+        if (other.gameObject.tag != "Marble" && other.gameObject.tag != "PlayerMarble")
+            return;
 
         if (transform.localScale.x > other.transform.localScale.x)
         {
-
-            if (gameObject.transform.localScale.x < maxMarbleSize)
+            if (gameObject.transform.localScale.x <= maxMarbleSize)
             {
                 Eat(other.gameObject);
                 Respawn(other.gameObject);
@@ -47,22 +48,29 @@ public class MoveMarble : MonoBehaviour
     }
         private void Eat(GameObject marble)
         {
+            if (marble.CompareTag("PlayerMarble"))
+            {
+                Debug.Log("Player Eaten");
+            }
             if (marble.CompareTag("Marble"))
             {
                 marble.gameObject.SetActive(false);
 
-                //Mix the scales
-                float newScale = transform.localScale.x + (marble.transform.localScale.x * growthRate);
-                if (newScale > maxMarbleSize) newScale = maxMarbleSize;
-                transform.localScale = new Vector3(newScale, newScale, newScale);
-                Debug.Log(marble.gameObject.name + " is Destroyed");
+            //Mix the scales
+            float newScale = transform.localScale.x + Mathf.Abs((marble.transform.localScale.x) * growthRate);
+            if (newScale > maxMarbleSize) 
+                newScale = maxMarbleSize;
+                
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+            Debug.Log(marble.gameObject.name + " is Destroyed");
 
-                //Mix the colors
-                //Color newColor = (GetComponent<Renderer>().material.color + marble.gameObject.GetComponent<Renderer>().material.color * 0.33f) / 2;
-                //GetComponent<Renderer>().material.color = newColor;
-                ChangeColor(Agent.Instance.Scale);
-                //Mix the mass
-                GetComponent<Rigidbody>().mass = (GetComponent<Rigidbody>().mass + marble.gameObject.GetComponent<Rigidbody>().mass * growthRate);
+            //Mix the colors
+            //Color newColor = (GetComponent<Renderer>().material.color + marble.gameObject.GetComponent<Renderer>().material.color * 0.33f) / 2;
+            //GetComponent<Renderer>().material.color = newColor;
+            ChangeColor(Agent.Instance.Scale);
+
+            //Check the mass
+            GetComponent<Rigidbody>().mass = (GetComponent<Rigidbody>().mass + marble.gameObject.GetComponent<Rigidbody>().mass * growthRate);
             }
         }
         private Vector3 generateSpawnLocation()
@@ -73,7 +81,7 @@ public class MoveMarble : MonoBehaviour
             float z = Random.Range(-1f, 1f) * worldBoundry.localScale.z / 2 + worldBoundry.position.z;
             return new Vector3(x, y, z);
         }
-        private void Respawn(GameObject marble)
+        public void Respawn(GameObject marble)
         {
             Vector3 spawnLoc = generateSpawnLocation();
             marble.gameObject.transform.position = spawnLoc;
--- Assets/Scripts/MovePlayerMarble.cs
+++ Assets/Scripts/MovePlayerMarble.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class MovePlayerMarble : MonoBehaviour
 {
+    public float maxPlayerSize = 4;
+    public float growthRate = 0.01f;
+    private bool GameStart = false;
     private Vector3 direction = new Vector3(0, 0, 0);
     void Start()
     {
@@ -24,11 +27,9 @@ public class MovePlayerMarble : MonoBehaviour
 
         if (transform.localScale.x > other.transform.localScale.x)
         {
-            //Mix the scales
-            //Eat(other.gameObject);
+            Eat(other.gameObject);
+            other.gameObject.GetComponent<MoveMarble>().Respawn(other.gameObject);
             NotifyScaleChange();
-            //Mix the mass
-            GetComponent<Rigidbody>().mass = (GetComponent<Rigidbody>().mass + other.gameObject.GetComponent<Rigidbody>().mass * 0.33f);
         }
     }
     private void Eat(GameObject marble)
@@ -38,16 +39,15 @@ public class MovePlayerMarble : MonoBehaviour
             marble.gameObject.SetActive(false);
 
             //Mix the scales
-            float newScale = transform.localScale.x + (marble.transform.localScale.x * 0.33f);
+            float newScale = transform.localScale.x + Mathf.Abs((marble.transform.localScale.x) * growthRate);
+            if (newScale > maxPlayerSize) newScale = maxPlayerSize;
             transform.localScale = new Vector3(newScale, newScale, newScale);
-            Debug.Log(marble.gameObject.name + " is Destroyed");
-
-            //Mix the colors
-            Color newColor = (GetComponent<Renderer>().material.color + marble.gameObject.GetComponent<Renderer>().material.color * 0.33f) / 2;
-            GetComponent<Renderer>().material.color = newColor;
+            Debug.Log(marble.gameObject.name + " is Destroyed by the Player Ball");
 
             //Mix the mass
-            GetComponent<Rigidbody>().mass = (GetComponent<Rigidbody>().mass + marble.gameObject.GetComponent<Rigidbody>().mass * 0.33f);
+            GetComponent<Rigidbody>().mass = (GetComponent<Rigidbody>().mass + marble.gameObject.GetComponent<Rigidbody>().mass * growthRate);
+
+            marble.gameObject.GetComponent<MoveMarble>().Respawn(marble.gameObject);
         }
     }
     
