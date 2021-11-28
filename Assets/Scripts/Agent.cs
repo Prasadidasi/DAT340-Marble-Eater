08dd255725fcfac720afb6656090484a6830a0d4
Abhinav Prasad <psmithman@gmail.com>
2021-11-28 20:43:47 +0530

    Implemented Player Lives and Respawn
    Signed-off-by: Abhinav Prasad <psmithman@gmail.com>

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -12,6 +12,11 @@ public class Agent : MonoBehaviour
     public int PlayerMarbleLives { get; set; }
     public int GameStartTimer { get; set; }
     public int KilledMarbles { get; set; }
+    
+    //0 means player has started
+    //-1 means player died
+    //1 means player resurrected after dying
+    public int playerStatus { get; set; }
 
     public static Agent Instance;
     // Start is called before the first frame update
@@ -23,6 +28,7 @@ public class Agent : MonoBehaviour
         Instance.PlayerMarbleLives = 0;
         Instance.GameStartTimer = 0;
         Instance.KilledMarbles = 0;
+        Instance.playerStatus = 0;
     }
     
 }
